f31c1ab80a1da692ab1968395f2b248fae91fba7
OguzBayat <oguzbayat77@gmail.com>
2018-07-21 21:18:56 +0300

    create class

--- script.rb
+++ script.rb
@@ -1,1 +1,12 @@
+class Scholl
+	attr_reader :city, :name
 
+	def initialize city, name
+		@city = city
+		@name = name
+	end
+
+	my_scholl = Scholl.new("Karabük","Karabük Üniversitesi")
+
+	puts "#{my_scholl.name} from #{my_scholl.city}"
+end
