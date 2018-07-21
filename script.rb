011f6026ba36f230e7ef77cd35152c86935472c2
yclyldrm <yucellyildirim@gmail.com>
2018-07-21 22:37:52 +0300

    Create School class

--- script.rb
+++ script.rb
@@ -1,1 +1,12 @@
+class School
+	attr_reader :name,:city
 
+	def initialize(name,city)
+		@name = name
+		@city = city
+	end
+
+	college = School.new("Pamukkale Üniversitesi","Denizli")
+
+	puts "I study Computer Engineering at #{college.name},#{college.city}"
+end
