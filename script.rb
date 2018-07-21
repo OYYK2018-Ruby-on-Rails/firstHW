8bae42962aba5f5b6e8f50cb013ef1dcd70fd099
nafidurmus <nafidurmus07@gmail.com>
2018-07-21 21:48:24 +0300

    first homework

--- script.rb
+++ script.rb
@@ -1,1 +1,18 @@
+class School
+	attr_reader :name
+	attr_reader :city
+
+	def initialize(name, city)
+		@name = name
+		@city = city
+
+	end
+end
+
+
+student = School.new('nafi', 'Isparta')
+
+
+puts "Benim adım #{student.name}, yasadıgım sehir #{student.city}"
+
 
